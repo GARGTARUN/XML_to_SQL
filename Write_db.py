2bc3e0228262b1dc6bdf34525a02addd29d95533
tarun <gargtarun08@gmail.com>
2019-12-01 20:03:52 +0530

    version 2.0

--- Write_db.py
+++ Write_db.py
@@ -1,32 +1,60 @@
 import pandas as pd
 import xml.etree.ElementTree as et
 import sqlite3
+import pandas as pd
+import xml.etree.ElementTree as et
 
+
+def parse_XML(xml_file, df_cols):
+    xtree = et.parse(xml_file)
+    xroot = xtree.getroot()
+    rows = []
+
+    for node in xroot:
+        res = []
+        res.append(node.attrib.get(df_cols[0]))
+        for el in df_cols[1:]:
+            if node is not None and node.find(el) is not None:
+                res.append(node.find(el).text)
+            else:
+                res.append(None)
+        rows.append({df_cols[i]: res[i]
+                     for i, _ in enumerate(df_cols)})
+
+    out_df = pd.DataFrame(rows, columns=df_cols)
+
+    return out_df
 # connecting to the database
+import tkinter as tk
+from tkinter import filedialog
 
-connection = sqlite3.connect("myTable.db")
+root = tk.Tk()
+root.withdraw()
 
+file_path = filedialog.askopenfilename()
 
-xtree = et.parse("emp.xml")
-xroot = xtree.getroot()
+print("Enter Table name:",sep='')
+table_name=input()
+# list of all the tags
+elemList=[]
+xtree = et.parse(file_path)
+for elem in xtree.iter():
+    if elem.tag in elemList:
+        break
+    elemList.append(elem.tag)
 
-df_cols = ["first_name", "last_name", "title", "division"]
-rows = []
+elemList.pop(0)
+elemList.pop(0)
+elemList.insert(0,"none")
+#print(elemList)
+# dataframe of all the data
 
-for node in xroot:
-    e_name = node.find("firstname").text if node is not None else None
-    e_lname = node.find("lastname").text if node is not None else None
-    e_title = node.find("title").text if node is not None else None
-    e_div = node.find("division").text if node is not None else None
-
-    rows.append({"first_name": e_name, "last_name": e_lname,
-                 "title": e_title, "division": e_div})
-
-out_df = pd.DataFrame(rows, columns=df_cols)
-
-df = pd.DataFrame(out_df)
-df.to_sql("emp", connection, if_exists="replace")
+df= parse_XML(file_path,elemList)
+df = df.drop(columns="none")
+print(df.head(6))
+connection = sqlite3.connect(table_name+".db")
+df.to_sql(table_name, connection, if_exists="replace")
 connection.commit()
 connection.close()
-print((df.head(6)))
+
 
--- read_db.py
+++ read_db.py
@@ -1,13 +1,22 @@
 import sqlite3
 
+import tkinter as tk
+from tkinter import filedialog
+
+root = tk.Tk()
+root.withdraw()
+
+file_path = filedialog.askopenfilename()
 # connect withe the myTable database
-connection = sqlite3.connect("myTable.db")
+connection = sqlite3.connect(file_path)
+print("Enter Table name:",sep='')
+t_name=input()
 
 # cursor object
 crsr = connection.cursor()
 
 # execute the command to fetch all the data from the table emp
-crsr.execute("SELECT * FROM emp")
+crsr.execute("SELECT * FROM "+t_name)
 
 # store all the fetched data in the ans variable
 ans = crsr.fetchall()
