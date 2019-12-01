91871e3142e52d2de06797a78f4691e29f13657e
tarun <gargtarun08@gmail.com>
2019-12-01 20:21:24 +0530

    no need to enter table neam in read

--- read_db.py
+++ read_db.py
@@ -9,14 +9,22 @@ root.withdraw()
 file_path = filedialog.askopenfilename()
 # connect withe the myTable database
 connection = sqlite3.connect(file_path)
-print("Enter Table name:",sep='')
-t_name=input()
+
+for i in range(len(file_path)-1,0,-1):
+    if "." ==file_path[i]:
+        end=i
+    if  '/' ==file_path[i]:
+        st=i+1
+        break
+file_path=file_path[st:end]
+
+print(file_path)
 
 # cursor object
 crsr = connection.cursor()
 
 # execute the command to fetch all the data from the table emp
-crsr.execute("SELECT * FROM "+t_name)
+crsr.execute("SELECT * FROM "+file_path)
 
 # store all the fetched data in the ans variable
 ans = crsr.fetchall()
